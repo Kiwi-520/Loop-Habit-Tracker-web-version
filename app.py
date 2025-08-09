9a357e952f0187a8e65fc9387d210aad4b16dac3
DishaHolmukhe <dishaholmukhe55@gmail.com>
2025-08-09 23:53:20 +0530

    Initial commit: Add Streamlit Habit Tracker application

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title = "Habit Tracker", layout = 'wide')
+st.title('Welcome to your Habit Tracker!')
+st.write('Use the sidebar to navigate')
---
+++ pages/Statistics.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+st.title('Statistics')
+st.write('Visualization and statistics will appear here.')
